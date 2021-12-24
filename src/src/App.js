adcb3d17a5be5a2008362e170c6234d60b45aed7
ajnatarajan <ajnatarajan@gmail.com>
2021-12-24 11:45:34 -0800

    navbar done, carousel is work in progress

--- src/src/App.js
+++ src/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import TopBar from './components/TopBar';
+import MainCarousel from './components/MainCarousel';
 
 export default function App() {
     return (
+      <div>
         <TopBar />
-        
+        <MainCarousel />
+      </div>
     );
 }
--- src/src/components/TopBar.js
+++ src/src/components/TopBar.js
@@ -7,8 +7,12 @@ export default function TopBar() {
     return(
         <header>
           <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous"></link>
-          <nav className="navbar navbar-expand-lg dark-theme py-3">
+          <nav className="navbar navbar-expand-lg dark-theme py-3 flex-container">
             <OurButton type='button'>Attempt 1</OurButton>
+            <div className="navbar-title-text">
+                Ajay and Eugene
+            </div>
+            <OurButton type='button'>Say Hi!</OurButton>
           </nav>
         </header>
       );

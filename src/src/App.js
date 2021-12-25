8118664c501eed477f73a1236a02e57f678391fa
ajnatarajan <ajnatarajan@gmail.com>
2021-12-25 13:21:35 -0800

    yinyang symbol added

--- src/src/App.js
+++ src/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import TopBar from './components/TopBar';
+import yin_yang from './img/yinyang.png';
+import yin_yang2 from './img/yinyang2.png';
 
 export default function App() {
     return (
       <div className="page-container">
         <TopBar />
+        <div className="landing-main-area mt-5">
+          <img src={yin_yang2} className="yinyang"/>
+        </div>
       </div>
     );
 }

4991f5f4fb4125b1651183f115454993ba27d446
ajnatarajan <ajnatarajan@gmail.com>
2021-12-25 13:33:51 -0800

    added names

--- src/src/App.js
+++ src/src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import TopBar from './components/TopBar';
 import yin_yang from './img/yinyang.png';
 import yin_yang2 from './img/yinyang2.png';
+import yin_yang3 from './img/yinyang3.png';
 
 export default function App() {
     return (
       <div className="page-container">
         <TopBar />
         <div className="landing-main-area mt-5">
-          <img src={yin_yang2} className="yinyang"/>
+          <img src={yin_yang3} className="yinyang"/>
         </div>
       </div>
     );

45bf54de7fefc5d17c3bbef0961bb2c6ca66111a
ajnatarajan <ajnatarajan@gmail.com>
2021-12-26 11:21:15 -0800

    added individual halves of yinyang

--- src/src/App.js
+++ src/src/App.js
@@ -1,15 +1,19 @@
 import './App.css';
 import TopBar from './components/TopBar';
 import yin_yang from './img/yinyang.png';
-import yin_yang2 from './img/yinyang2.png';
-import yin_yang3 from './img/yinyang3.png';
+import yin_yang_black from './img/yinyangblack.png';
+import yin_yang_white from './img/yinyangwhite.png';
 
 export default function App() {
     return (
       <div className="page-container">
         <TopBar />
         <div className="landing-main-area mt-5">
-          <img src={yin_yang3} className="yinyang"/>
+          <img src={yin_yang} className="yinyang" type="button" onclick={alert("Testing")}/>
+          <div className="composite-yin-yang">
+            <img src={yin_yang_black} className="yinyang"/>
+            <img src={yin_yang_white} className="yinyang"/>
+          </div>
         </div>
       </div>
     );
